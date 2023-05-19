c21445c6634ae4fd3a21915824bd01aeef349826
ITTYAMU <125099500+ITTYAMU@users.noreply.github.com>
2023-05-19 09:37:13 +0900

    Add files via upload

--- BC/js/myscript3.js
+++ BC/js/myscript3.js
@@ -1,6 +1,8 @@
 let Data = {"URL": ""}
 let text = document.getElementById('test').innerText;
 
+var xhr = new XMLHttpRequest();
+
 chrome.tabs.query({
     active: true,
     currentWindow: true,
@@ -8,18 +10,15 @@ chrome.tabs.query({
     const tab = tabs[0]
     Data.URL = tab.url;
     console.log(`URL: ${Data.URL}`);
-
     
+
+
     var url_text = String(`URL: ${Data.URL}`);
-    document.getElementById('test').innerText = url_text;
+    document.getElementById('test').innerText = url_text
 });
 
+xhr.open('POST','http://52.194.67.163', true);
+xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded');
+xhr.send('url=ハッサンのおちんちん')
 
-
-
-window.addEventListener('load',()=>{
-    const txtBox = document.querySelector('input');
-    document.querySelector('button.url').addEventListener('click',()=>{
-        txtBox.value = Data.URL;
-    });
-})
+//ここからPOSTがおくれないなぞすぎる
