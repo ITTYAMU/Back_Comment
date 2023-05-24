de97192e11999a8a6a7811ab9d1818cdfbcf7a67
ITTYAMU <125099500+ITTYAMU@users.noreply.github.com>
2023-05-24 15:54:57 +0900

    Add files via upload

--- BC/js/myscript3.js
+++ BC/js/myscript3.js
@@ -11,14 +11,12 @@ chrome.tabs.query({
     Data.URL = tab.url;
     console.log(`URL: ${Data.URL}`);
     
-
-
     var url_text = String(`URL: ${Data.URL}`);
     document.getElementById('test').innerText = url_text
 });
 
 xhr.open('POST','http://52.194.67.163/BackComment/notePad.html', true);
 xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded');
-xhr.send('url=')
+xhr.send('url=キツツキ')
 
 //ここからPOSTがおくれないなぞすぎる
